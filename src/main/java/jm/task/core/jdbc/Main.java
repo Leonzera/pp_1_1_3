0406a0431d7ea3056ec7c22a27acb1eb81fe3ba5
Ева <eviksa13@mail.ru>
2026-04-06 13:19:07 +0300

    исправлено

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,29 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.util.Util;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Util.getConnection();
 
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 22);
+        userService.saveUser("Олег", "Олегов", (byte) 21);
+        userService.saveUser("Гело", "Вгело", (byte) 35);
+        userService.saveUser("Нави", "Петров", (byte) 24);
+
+        List<User> users = userService.getAllUsers();
+        System.out.println("\n---- Список всех пользователей ----");
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
